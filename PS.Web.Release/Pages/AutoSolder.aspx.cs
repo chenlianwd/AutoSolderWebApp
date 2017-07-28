da727dfcd443602581fb745f6d801ff482f7bda8
chenlianwd <chenlianwd@163.com>
2017-07-28 09:20:40 +0800

    first push

---
+++ PS.Web.Release/Pages/AutoSolder.aspx.cs
@@ -0,0 +1,22 @@
+﻿using PS;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Pages_AutoSolder : System.Web.UI.Page
+{
+    protected LanguageHelper LangHelper = null;
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        //查看是否有用户级语言辅助器，没有则用公共的
+        LangHelper = FetchData.GetLanguageHelper(Session);
+    }
+
+    protected void settingbtn1_Click(object sender, EventArgs e)
+    {
+
+    }
+}
---
+++ PS.Web.Release/Pages/AutoSolderDetail.aspx.cs
@@ -0,0 +1,38 @@
+﻿
+using PS;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Pages_AutoSolderDetail : System.Web.UI.Page
+{
+    protected LanguageHelper LangHelper = null;
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        //查看是否有用户级语言辅助器，没有则用公共的
+        LangHelper = FetchData.GetLanguageHelper(Session);
+
+        DateTime dtEnd = DateTime.Now.AddDays(1);
+        string sTime = Request["time"];
+        if (!string.IsNullOrEmpty(sTime))
+            dtEnd = DateTime.Parse(sTime).AddMinutes(1);
+
+        DateTime dtStart = dtEnd.AddDays(-30);
+            //dtEnd.AddHours(-2);
+            //
+
+        edtStartTime.Text = dtStart.ToString("yyyy-MM-dd HH:mm");
+        edtEndTime.Text = dtEnd.ToString("yyyy-MM-dd HH:mm");
+        edtStation.Text = Request["line"];
+
+        string test = Request["Remain"];
+    }
+
+    
+    
+
+   
+}
